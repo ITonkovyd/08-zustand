8218b9ba6fe084065b87a52fa8274bbf3cce610e
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-09-23 22:16:11 +0300

    Update: removing formik

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,93 +1,88 @@
 "use client";
 
-import { Field, Form, Formik, ErrorMessage } from "formik";
-import * as Yup from "yup";
+import { useRouter } from "next/navigation";
 import { useNoteMutation } from "@/hooks/useNoteMutation";
 import { createNote } from "@/lib/api";
-import type { BaseNoteParams } from "@/types/note";
+import { BaseNoteParams, noteTag } from "@/types/note";
 import css from "./NoteForm.module.css";
 
-interface NoteFormProps {
-  onClose: () => void;
-}
 
-const NoteForm = ({ onClose }: NoteFormProps) => {
-  const formValidationSchema = Yup.object().shape({
-    title: Yup.string()
-      .min(3, "Too Short!")
-      .max(50, "Too Long!")
-      .required("Title is required"),
-    content: Yup.string().max(500, "Too Long!"),
-    tag: Yup.string()
-      .oneOf(["Work", "Personal", "Shopping", "Meeting", "Todo"])
-      .required("Tag is required"),
-  });
+const TAGS: noteTag[] = ["Todo", "Work", "Personal", "Meeting", "Shopping"];
 
-  const createNoteMutation = useNoteMutation({
-    mutationFn: (values: BaseNoteParams) => createNote(values),
+const NoteForm = () => {
+  const router = useRouter();
+
+  const { mutate } = useNoteMutation<BaseNoteParams>({
+    mutationFn: (newNote: BaseNoteParams) => createNote(newNote),
     queryKey: ["notes"],
     successMsg: "Note created successfully",
     errorMsg: "Error creating note",
-    successAction: onClose,
+    successAction: () => router.push("/notes/filter/All"),
   });
 
-  const handleSubmitForm = (values: BaseNoteParams) => {
-    createNoteMutation.mutate(values);
+  const backToPreviousPage = () => {
+    router.back();
+  };
+
+  const handleSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+    const tag = formData.get("tag") as noteTag;
+
+    const values: BaseNoteParams = { title, content, tag };
+    mutate(values);
   };
 
   return (
-    <Formik
-      initialValues={{ title: "", content: "", tag: "Todo" }}
-      validationSchema={formValidationSchema}
-      onSubmit={handleSubmitForm}
-    >
-      <Form className={css.form}>
-        <div className={css.formGroup}>
-          <label htmlFor="title">Title</label>
-          <Field id="title" type="text" name="title" className={css.input} />
-          <ErrorMessage name="title">
-            {(errorMsg) => <span className={css.error}>{errorMsg}</span>}
-          </ErrorMessage>
-        </div>
+    <form action={handleSubmit} className={css.form}>
+      <div className={css.formGroup}>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          className={css.input}
+          required
+          minLength={3}
+          maxLength={50}
+        />
+      </div>
 
-        <div className={css.formGroup}>
-          <label htmlFor="content">Content</label>
-          <Field
-            as="textarea"
-            id="content"
-            name="content"
-            rows={8}
-            className={css.textarea}
-          />
-          <ErrorMessage name="content">
-            {(errorMsg) => <span className={css.error}>{errorMsg}</span>}
-          </ErrorMessage>
-        </div>
+      <div className={css.formGroup}>
+        <label htmlFor="content">Content</label>
+        <textarea
+          id="content"
+          name="content"
+          rows={8}
+          className={css.textarea}
+          maxLength={500}
+        />
+      </div>
 
-        <div className={css.formGroup}>
-          <label htmlFor="tag">Tag</label>
-          <Field as="select" id="tag" name="tag" className={css.select}>
-            <option value="Todo">Todo</option>
-            <option value="Work">Work</option>
-            <option value="Personal">Personal</option>
-            <option value="Meeting">Meeting</option>
-            <option value="Shopping">Shopping</option>
-          </Field>
-          <ErrorMessage name="tag">
-            {(errorMsg) => <span className={css.error}>{errorMsg}</span>}
-          </ErrorMessage>
-        </div>
+      <div className={css.formGroup}>
+        <label htmlFor="tag">Tag</label>
+        <select id="tag" name="tag" className={css.select} required>
+          {TAGS.map((tag) => (
+            <option key={tag} value={tag}>
+              {tag}
+            </option>
+          ))}
+        </select>
+      </div>
 
-        <div className={css.actions}>
-          <button type="button" className={css.cancelButton} onClick={onClose}>
-            Cancel
-          </button>
-          <button type="submit" className={css.submitButton} disabled={false}>
-            Create note
-          </button>
-        </div>
-      </Form>
-    </Formik>
+      <div className={css.actions}>
+        <button
+          type="button"
+          className={css.cancelButton}
+          onClick={backToPreviousPage}
+        >
+          Cancel
+        </button>
+        <button type="submit" className={css.submitButton}>
+          Create note
+        </button>
+      </div>
+    </form>
   );
 };
 
