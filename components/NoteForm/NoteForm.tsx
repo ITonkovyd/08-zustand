09c2ed6809d8357d908e346d2d2bcf2691955e26
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-09-30 20:10:10 +0300

    Update: implementing zustand to creating new note form

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -3,14 +3,15 @@
 import { useRouter } from "next/navigation";
 import { useNoteMutation } from "@/hooks/useNoteMutation";
 import { createNote } from "@/lib/api";
+import { useNoteDraftStore } from "@/lib/stores/noteStore";
 import { BaseNoteParams, noteTag } from "@/types/note";
 import css from "./NoteForm.module.css";
-
 
 const TAGS: noteTag[] = ["Todo", "Work", "Personal", "Meeting", "Shopping"];
 
 const NoteForm = () => {
   const router = useRouter();
+  const { draft, setDraft, clearDraft } = useNoteDraftStore();
 
   const { mutate } = useNoteMutation<BaseNoteParams>({
     mutationFn: (newNote: BaseNoteParams) => createNote(newNote),
@@ -24,6 +25,15 @@ const NoteForm = () => {
     router.back();
   };
 
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setDraft({ ...draft, [name]: value });
+  };
+
   const handleSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
     const content = formData.get("content") as string;
@@ -31,6 +41,7 @@ const NoteForm = () => {
 
     const values: BaseNoteParams = { title, content, tag };
     mutate(values);
+    clearDraft();
   };
 
   return (
@@ -41,10 +52,12 @@ const NoteForm = () => {
           id="title"
           type="text"
           name="title"
+          defaultValue={draft?.title}
           className={css.input}
           required
           minLength={3}
           maxLength={50}
+          onChange={handleChange}
         />
       </div>
 
@@ -53,15 +66,24 @@ const NoteForm = () => {
         <textarea
           id="content"
           name="content"
+          defaultValue={draft?.content}
           rows={8}
           className={css.textarea}
           maxLength={500}
+          onChange={handleChange}
         />
       </div>
 
       <div className={css.formGroup}>
         <label htmlFor="tag">Tag</label>
-        <select id="tag" name="tag" className={css.select} required>
+        <select
+          id="tag"
+          name="tag"
+          defaultValue={draft?.tag}
+          className={css.select}
+          required
+          onChange={handleChange}
+        >
           {TAGS.map((tag) => (
             <option key={tag} value={tag}>
               {tag}
